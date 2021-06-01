86d00a43a71fdbb8ca860524fd0cfd1b51136fde
Savin <savingoyal@gmail.com>
2021-06-01 09:58:57 -0700

    Add Unbounded Foreach support

--- metaflow/plugins/__init__.py
+++ metaflow/plugins/__init__.py
@@ -97,7 +97,10 @@ from .timeout_decorator import TimeoutDecorator
 from .environment_decorator import EnvironmentDecorator
 from .retry_decorator import RetryDecorator
 from .aws.batch.batch_decorator import BatchDecorator, ResourcesDecorator
-from .aws.step_functions.step_functions_decorator import StepFunctionsInternalDecorator
+from .aws.step_functions.step_functions_decorator \
+                import StepFunctionsInternalDecorator
+from .test_unbounded_foreach_decorator\
+    import InternalTestUnboundedForeachDecorator, InternalTestUnboundedForeachInput
 from .conda.conda_step_decorator import CondaStepDecorator
 
 STEP_DECORATORS = _merge_lists([CatchDecorator,
@@ -107,7 +110,9 @@ STEP_DECORATORS = _merge_lists([CatchDecorator,
                                 RetryDecorator,
                                 BatchDecorator,
                                 StepFunctionsInternalDecorator,
-                                CondaStepDecorator], _ext_plugins.STEP_DECORATORS, 'name')
+                                CondaStepDecorator,
+                                InternalTestUnboundedForeachDecorator],
+                                    _ext_plugins.STEP_DECORATORS, 'name')
 
 # Add Conda environment
 from .conda.conda_environment import CondaEnvironment
