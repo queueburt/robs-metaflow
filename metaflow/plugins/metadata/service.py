Netflix/metaflow

Add after-execution tagging support (both in client and CLI)
You can now modify tags after a run has executed. A CLI provides
the ability to see all the tags, add or remove them.

The client also allows you to operate on tags directly for all
objects.

Metadata service will need to be updated to version 2.0.6 for this
to work with the metadata service. This currently works as is with
local metadata.

metaflow/plugins/metadata/service.py
@@ -1,10 +1,11 @@
+from metaflow.plugins.aws.step_functions.step_functions_cli import IncorrectMetadataServiceVersion
 import os
 import requests
 import time
 
 from distutils.version import LooseVersion
 
-from metaflow.exception import MetaflowException
+from metaflow.exception import MetaflowException, TaggingException
 from metaflow.metaflow_config import METADATA_SERVICE_NUM_RETRIES, METADATA_SERVICE_HEADERS, \
     METADATA_SERVICE_URL
 from metaflow.metadata import MetadataProvider
@@ -28,6 +29,8 @@ class ServiceException(MetaflowException):
 
 class ServiceMetadataProvider(MetadataProvider):
     TYPE = 'service'
+
+    _can_perform_tagging = None
 
     def __init__(self, environment, flow, event_logger, monitor):
         super(ServiceMetadataProvider, self).__init__(environment, flow, event_logger, monitor)
@@ -183,6 +186,27 @@ class ServiceMetadataProvider(MetadataProvider):
             if ex.http_code == 404:
                 return None
             raise
+
+    @classmethod
+    def _perform_operations_internal(cls, operations):
+        if cls._can_perform_tagging is None:
+            cls._can_perform_tagging = cls._version(None) is not None and \
+            LooseVersion(cls._version(None)) >= LooseVersion('2.0.6')
+        if not cls._can_perform_tagging:
+            raise IncorrectMetadataServiceVersion(
+                "Tagging not supported on this version of Metaflow service. "
+                "Try again with a more recent version of metaflow service "
+                "(>= 2.0.6)")
+        to_send = []
+        for op in operations:
+            if op.op_type != 'tags':
+                raise ValueError("Only tag operations are supported")
+            to_send.append({
+                "object_type": op.object_type,
+                "id": op.id,
+                "operation": op.operation,
+                "tag": op.args['tag']})
+        return cls._request(None, "/tags", data=to_send)
 
     def _new_run(self, run_id=None, tags=[], sys_tags=[]):
         # first ensure that the flow exists

queueburt/robs-metaflow

metaflow/plugins/metadata/service.py
@@ -1,10 +1,11 @@
+import json
 import os
 import requests
 import time
 
 from distutils.version import LooseVersion
 
-from metaflow.exception import MetaflowException
+from metaflow.exception import MetaflowException, TaggingException
 from metaflow.metaflow_config import METADATA_SERVICE_NUM_RETRIES, METADATA_SERVICE_HEADERS, \
     METADATA_SERVICE_URL
 from metaflow.metadata import MetadataProvider
@@ -28,6 +29,8 @@ class ServiceException(MetaflowException):
 
 class ServiceMetadataProvider(MetadataProvider):
     TYPE = 'service'
+
+    _can_perform_tagging = None
 
     def __init__(self, environment, flow, event_logger, monitor):
         super(ServiceMetadataProvider, self).__init__(environment, flow, event_logger, monitor)
@@ -54,10 +57,10 @@ class ServiceMetadataProvider(MetadataProvider):
     def version(self):
         return self._version(self._monitor)
 
-    def new_run_id(self, tags=[], sys_tags=[]):
+    def new_run_id(self, tags=None, sys_tags=None):
         return self._new_run(tags=tags, sys_tags=sys_tags)
 
-    def register_run_id(self, run_id, tags=[], sys_tags=[]):
+    def register_run_id(self, run_id, tags=None, sys_tags=None):
         try:
             # don't try to register an integer ID which was obtained
             # from the metadata service in the first place
@@ -66,15 +69,15 @@ class ServiceMetadataProvider(MetadataProvider):
         except ValueError:
             return self._new_run(run_id, tags=tags, sys_tags=sys_tags)
 
-    def new_task_id(self, run_id, step_name, tags=[], sys_tags=[]):
+    def new_task_id(self, run_id, step_name, tags=None, sys_tags=None):
         return self._new_task(run_id, step_name, tags=tags, sys_tags=sys_tags)
 
     def register_task_id(self,
                          run_id,
                          step_name,
                          task_id,
-                         tags=[],
-                         sys_tags=[]):
+                         tags=None,
+                         sys_tags=None):
         try:
             # don't try to register an integer ID which was obtained
             # from the metadata service in the first place
@@ -184,7 +187,28 @@ class ServiceMetadataProvider(MetadataProvider):
                 return None
             raise
 
-    def _new_run(self, run_id=None, tags=[], sys_tags=[]):
+    @classmethod
+    def _perform_operations_internal(cls, operations):
+        if cls._can_perform_tagging is None:
+            cls._can_perform_tagging = cls._version(None) is not None and \
+            LooseVersion(cls._version(None)) >= LooseVersion('2.0.6')
+        if not cls._can_perform_tagging:
+            raise MetaflowException(
+                "Tagging not supported on this version of Metaflow service. "
+                "Try again with a more recent version of metaflow service "
+                "(>= 2.0.6)")
+        to_send = []
+        for op in operations:
+            if op.op_type != 'tags':
+                raise ValueError("Only tag operations are supported")
+            to_send.append({
+                "object_type": op.object_type,
+                "id": op.id,
+                "operation": op.operation,
+                "tag": op.args['tag']})
+        return json.loads(cls._request(None, "/tags", data=to_send))
+
+    def _new_run(self, run_id=None, tags=None, sys_tags=None):
         # first ensure that the flow exists
         self._get_or_create('flow')
         run = self._get_or_create('run', run_id, tags=tags, sys_tags=sys_tags)
@@ -194,8 +218,8 @@ class ServiceMetadataProvider(MetadataProvider):
                   run_id,
                   step_name,
                   task_id=None,
-                  tags=[],
-                  sys_tags=[]):
+                  tags=None,
+                  sys_tags=None):
         # first ensure that the step exists
         self._get_or_create('step', run_id, step_name)
         task = self._get_or_create('task', run_id, step_name, task_id, tags=tags, sys_tags=sys_tags)
@@ -229,7 +253,12 @@ class ServiceMetadataProvider(MetadataProvider):
         return create_path + '/task'
 
     def _get_or_create(
-            self, obj_type, run_id=None, step_name=None, task_id=None, tags=[], sys_tags=[]):
+            self, obj_type, run_id=None, step_name=None, task_id=None, tags=None, sys_tags=None):
+
+        if tags is None:
+            tags = set()
+        if sys_tags is None:
+            sys_tags = set()
 
         def create_object():
             data = self._object_to_json(
@@ -237,8 +266,8 @@ class ServiceMetadataProvider(MetadataProvider):
                 run_id,
                 step_name,
                 task_id,
-                tags + self.sticky_tags,
-                sys_tags + self.sticky_sys_tags)
+                self.sticky_tags.union(tags),
+                self.sticky_sys_tags.union(sys_tags))
             return self._request(self._monitor, create_path, data, obj_path)
 
         always_create = False